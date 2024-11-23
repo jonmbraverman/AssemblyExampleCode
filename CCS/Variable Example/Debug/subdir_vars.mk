8c0b80030957d44f0644ee973411f26b242208c3
Jonathan Braverman <jon.m.braverman@gmail.com>
2024-11-23 14:06:23 -0500

    CCS Variables Example and Project

---
+++ CCS/Variable Example/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+ASM_SRCS += \
+C:/Users/jonmb/OneDrive\ -\ CCSU/Classapps/code/Assembly/CCS/ex1_variables.asm 
+
+OBJS += \
+./ex1_variables.obj 
+
+ASM_DEPS += \
+./ex1_variables.d 
+
+OBJS__QUOTED += \
+"ex1_variables.obj" 
+
+ASM_DEPS__QUOTED += \
+"ex1_variables.d" 
+
+ASM_SRCS__QUOTED += \
+"C:/Users/jonmb/OneDrive - CCSU/Classapps/code/Assembly/CCS/ex1_variables.asm" 
+
+
