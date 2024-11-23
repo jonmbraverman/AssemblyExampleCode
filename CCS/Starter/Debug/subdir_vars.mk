51b5fe2bb7f58b06c8c618cf1d3de8febffad094
Jonathan Braverman <jon.m.braverman@gmail.com>
2024-11-23 14:17:33 -0500

    Added starter projects

---
+++ CCS/Starter/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+ASM_SRCS += \
+../ex0_starter.asm 
+
+OBJS += \
+./ex0_starter.obj 
+
+ASM_DEPS += \
+./ex0_starter.d 
+
+OBJS__QUOTED += \
+"ex0_starter.obj" 
+
+ASM_DEPS__QUOTED += \
+"ex0_starter.d" 
+
+ASM_SRCS__QUOTED += \
+"../ex0_starter.asm" 
+
+
